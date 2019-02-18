d4e65f4d5845b90ad836afb850343384e8f708c6
Spike <spike.k25@gmail.com>
2019-02-18 22:12:11 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
 module.exports = function warmup(temperature) {
-    /* formula from wiki */
-    const formula = 9 / 5 + 32;
-
-    return temperature * formula;
+  return temperature * 9 / 5 + 32;
 };
